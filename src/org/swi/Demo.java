9510404be5700814c8d27c6e729a1e5d5d7232ce
Mukil24 <mukilchwt@gmail.com>
2022-09-09 14:27:04 +0530

    switch12

---
+++ src/org/swi/Demo.java
@@ -0,0 +1,37 @@
+package org.swi;
+
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) throws mukilnotboundException {
+		
+		Scanner s =new Scanner(System.in);
+		String s1 =s.nextLine();
+		if (s.equals("mukil")) {
+			System.out.println("hi mukil,how are you?");
+
+		} else {
+		throw new mukilnotboundException();
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}}
---
+++ src/org/swi/mukilnotboundException.java
@@ -0,0 +1,9 @@
+package org.swi;
+
+public class mukilnotboundException extends Exception {
+public String getMessage() {
+	String s2="the user is not valid";
+	return s2;
+	
+}
+}
